2fca207717cd5fd9ffc51b4d503fbf89d7abd4f2
Zgzrock <zgzrock@hotmail.com>
2025-01-08 09:18:17 +0100

    Delete Cartel directory

--- Cartel/zz.bat
+++
@@ -1,9 +0,0 @@
-Dir *.*
-@echo off
-set outputFile=lista_archivos.txt
-rem Borrar el archivo de salida si ya existe
-if exist %outputFile% del %outputFile%
-rem Recorrer todos los archivos en el directorio actual
-for %%f in (*) do echo %%f >> %outputFile%
-echo Listado de archivos guardado en %outputFile%
-pause
